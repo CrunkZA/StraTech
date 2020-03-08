632416f7f8d074198749536befc024a174be7139
Luke Donald Boniface <boniface.luke@gmail.com>
2020-03-08 22:24:29 +0200

    origin

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,14 +5,14 @@ class Calculator{
         this.clear()
     }
 
-    clear(){
+    clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
-    }
+      }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0.-1)
     }
 
     appendNumber(number){
@@ -20,17 +20,58 @@ class Calculator{
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
-    chooseOperation(operation){
-
-    }
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+          this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+      }
 
     compute(){
+       let computation 
+       const prev = parseFloat(this.previousOperand)
+       const current = parseFloat(this.currentOperand)
+       if(isNaN(prev)|| isNaN(current))return
+       switch (this.operation)
+       {
+        case '+':
+           computation = prev + current
+           break
+
+        case '-':
+           computation = prev - current
+           break
+
+        case '*':
+           computation = prev * current
+           break
+
+        case '÷':
+           computation = prev / current
+           break
+
+        default:
+          return
+       }
+       this.currentOperand = computation
+       this.operation = undefined
+       this.this.previousOperand = ''
 
     }
 
-    updateDisplay(){
-      this.currentOperandTextElement.innerText = this.currentOperand
-    }
+    updateDisplay() {
+        this.currentOperandTextElement.innerText =
+          this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+          this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+          this.previousOperandTextElement.innerText = ''
+        }
+      }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -50,8 +91,20 @@ numberButtons.forEach(button => {
    })
 })
 operationButtons.forEach(button => {
-    button.addEventListener('click', () => { 
-         calculator.appendNumber(button.innerText)
-         calculator.updateDisplay();
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
     })
  })
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+  })
+ allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+  })
+    deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
