20817b8c09584a352d6882c616ddaebe48b64e98
Luke Donald Boniface <boniface.luke@gmail.com>
2020-03-08 22:50:46 +0200

    finished

--- Calculator/script.js
+++ Calculator/script.js
@@ -85,6 +85,7 @@ updateDisplay() {
 }
 }
 
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
